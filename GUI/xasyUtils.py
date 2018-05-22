ec5bc5306a7daa7ddadf2d3a021e3b888a6ca32c
Supakorn Rassameemasmuang <rassamee@ualberta.ca>
2018-05-22 14:01:43 -0600

    Optimize some code for preview.

--- GUI/xasyUtils.py
+++ GUI/xasyUtils.py
@@ -3,3 +3,20 @@ def tryParse(val, typ=float):
         return typ(val)
     except ValueError:
         return None
+
+
+def listize(str, typ, delim='()'):
+    str = str.strip(delim)
+    raw_elem = str.split(',')
+    final_list = []
+    if isinstance(typ, (list, tuple)):
+        for i in range(len(raw_elem)):
+            if i < len(typ):
+                curr_typ = typ[i]
+            else:
+                curr_typ = typ[-1]
+            final_list.append(curr_typ(raw_elem[i].strip()))
+    else:
+        for elem in raw_elem:
+            final_list.append(typ(elem.strip()))
+    return final_list
